0b4746e117d7ec70a5e420433e3a9ec3cb31494e
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2020-01-09 23:41:57 +0900

    fmt

--- list/main.cpp
+++ list/main.cpp
@@ -1,263 +1,263 @@
+#include "list.cpp"
 #include <iostream>
 #include <string>
-#include "list.cpp"
 
 bool testAdd1() {
-    auto list = new List<std::string>;
-    if (!list->IsEmpty()) {
-        return false;
-    }
+  auto list = new List<std::string>;
+  if (!list->IsEmpty()) {
+    return false;
+  }
 
-    list->Add("a");
-    if (list->Get(0) != "a") {
-        return false;
-    }
+  list->Add("a");
+  if (list->Get(0) != "a") {
+    return false;
+  }
 
-    list->Add("b");
-    if (list->Get(0) != "a") {
-        return false;
-    }
-    if (list->Get(1) != "b") {
-        return false;
-    }
+  list->Add("b");
+  if (list->Get(0) != "a") {
+    return false;
+  }
+  if (list->Get(1) != "b") {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 bool testAdd2() {
-    auto list = new List<std::string>;
-    if (!list->IsEmpty()) {
-        return false;
-    }
-    list->Add("a");
-    list->Add("b");
-    if (list->Get(0) != "a") {
-        return false;
-    }
-    if (list->Get(1) != "b") {
-        return false;
-    }
+  auto list = new List<std::string>;
+  if (!list->IsEmpty()) {
+    return false;
+  }
+  list->Add("a");
+  list->Add("b");
+  if (list->Get(0) != "a") {
+    return false;
+  }
+  if (list->Get(1) != "b") {
+    return false;
+  }
 
-    list->Add(0, "c");
-    if (list->Get(0) != "c") {
-        return false;
-    }
-    if (list->Get(1) != "a") {
-        return false;
-    }
-    if (list->Get(2) != "b") {
-        return false;
-    }
+  list->Add(0, "c");
+  if (list->Get(0) != "c") {
+    return false;
+  }
+  if (list->Get(1) != "a") {
+    return false;
+  }
+  if (list->Get(2) != "b") {
+    return false;
+  }
 
-    list->Add(1, "d");
-    if (list->Get(0) != "c") {
-        return false;
-    }
-    if (list->Get(1) != "d") {
-        return false;
-    }
-    if (list->Get(2) != "a") {
-        return false;
-    }
-    if (list->Get(3) != "b") {
-        return false;
-    }
+  list->Add(1, "d");
+  if (list->Get(0) != "c") {
+    return false;
+  }
+  if (list->Get(1) != "d") {
+    return false;
+  }
+  if (list->Get(2) != "a") {
+    return false;
+  }
+  if (list->Get(3) != "b") {
+    return false;
+  }
 
-    list->Add(4, "e");
-    if (list->Get(0) != "c") {
-        return false;
-    }
-    if (list->Get(1) != "d") {
-        return false;
-    }
-    if (list->Get(2) != "a") {
-        return false;
-    }
-    if (list->Get(3) != "b") {
-        return false;
-    }
-    if (list->Get(4) != "e") {
-        return false;
-    }
+  list->Add(4, "e");
+  if (list->Get(0) != "c") {
+    return false;
+  }
+  if (list->Get(1) != "d") {
+    return false;
+  }
+  if (list->Get(2) != "a") {
+    return false;
+  }
+  if (list->Get(3) != "b") {
+    return false;
+  }
+  if (list->Get(4) != "e") {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 bool testContains() {
-    auto list = new List<std::string>;
-    list->Add("a");
-    list->Add("b");
+  auto list = new List<std::string>;
+  list->Add("a");
+  list->Add("b");
 
-    if (!list->Contains("a")) {
-        return false;
-    }
+  if (!list->Contains("a")) {
+    return false;
+  }
 
-    if (!list->Contains("b")) {
-        return false;
-    }
+  if (!list->Contains("b")) {
+    return false;
+  }
 
-    if (list->Contains("c")) {
-        return false;
-    }
+  if (list->Contains("c")) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 bool testIsEmpty() {
-    auto list = new List<std::string>;
-    if (!list->IsEmpty()) {
-        return false;
-    }
+  auto list = new List<std::string>;
+  if (!list->IsEmpty()) {
+    return false;
+  }
 
-    list->Add("a");
-    if (list->IsEmpty()) {
-        return false;
-    }
+  list->Add("a");
+  if (list->IsEmpty()) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 bool testRemove() {
-    auto list = new List<std::string>;
-    list->Add("a");
-    list->Add("b");
-    list->Add("c");
-    if (list->Size() != 3) {
-        return false;
-    }
+  auto list = new List<std::string>;
+  list->Add("a");
+  list->Add("b");
+  list->Add("c");
+  if (list->Size() != 3) {
+    return false;
+  }
 
-    list->Remove(0);
-    if (list->Size() != 2) {
-        return false;
-    }
-    if (list->Get(0) != "b") {
-        return false;
-    }
-    if (list->Get(1) != "c") {
-        return false;
-    }
+  list->Remove(0);
+  if (list->Size() != 2) {
+    return false;
+  }
+  if (list->Get(0) != "b") {
+    return false;
+  }
+  if (list->Get(1) != "c") {
+    return false;
+  }
 
-    list->Remove(2);
-    if (list->Size() != 2) {
-        return false;
-    }
-    if (list->Get(0) != "b") {
-        return false;
-    }
-    if (list->Get(1) != "c") {
-        return false;
-    }
+  list->Remove(2);
+  if (list->Size() != 2) {
+    return false;
+  }
+  if (list->Get(0) != "b") {
+    return false;
+  }
+  if (list->Get(1) != "c") {
+    return false;
+  }
 
-    list->Remove(-1);
-    if (list->Size() != 2) {
-        return false;
-    }
-    if (list->Get(0) != "b") {
-        return false;
-    }
-    if (list->Get(1) != "c") {
-        return false;
-    }
+  list->Remove(-1);
+  if (list->Size() != 2) {
+    return false;
+  }
+  if (list->Get(0) != "b") {
+    return false;
+  }
+  if (list->Get(1) != "c") {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 bool testSet() {
-    auto list = new List<std::string>;
-    list->Add("a");
-    list->Add("b");
-    list->Add("c");
-    if (list->Size() != 3) {
-        return false;
-    }
+  auto list = new List<std::string>;
+  list->Add("a");
+  list->Add("b");
+  list->Add("c");
+  if (list->Size() != 3) {
+    return false;
+  }
 
-    list->Set(0, "d");
-    if (list->Size() != 3) {
-        return false;
-    }
-    if (list->Get(0) != "d") {
-        return false;
-    }
-    if (list->Get(1) != "b") {
-        return false;
-    }
-    if (list->Get(2) != "c") {
-        return false;
-    }
+  list->Set(0, "d");
+  if (list->Size() != 3) {
+    return false;
+  }
+  if (list->Get(0) != "d") {
+    return false;
+  }
+  if (list->Get(1) != "b") {
+    return false;
+  }
+  if (list->Get(2) != "c") {
+    return false;
+  }
 
-    list->Set(-1, "e");
-    if (list->Size() != 3) {
-        return false;
-    }
-    if (list->Get(0) != "d") {
-        return false;
-    }
-    if (list->Get(1) != "b") {
-        return false;
-    }
-    if (list->Get(2) != "c") {
-        return false;
-    }
+  list->Set(-1, "e");
+  if (list->Size() != 3) {
+    return false;
+  }
+  if (list->Get(0) != "d") {
+    return false;
+  }
+  if (list->Get(1) != "b") {
+    return false;
+  }
+  if (list->Get(2) != "c") {
+    return false;
+  }
 
-    list->Set(3, "f");
-    if (list->Size() != 3) {
-        return false;
-    }
-    if (list->Get(0) != "d") {
-        return false;
-    }
-    if (list->Get(1) != "b") {
-        return false;
-    }
-    if (list->Get(2) != "c") {
-        return false;
-    }
+  list->Set(3, "f");
+  if (list->Size() != 3) {
+    return false;
+  }
+  if (list->Get(0) != "d") {
+    return false;
+  }
+  if (list->Get(1) != "b") {
+    return false;
+  }
+  if (list->Get(2) != "c") {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 bool testSize() {
-    auto list = new List<std::string>;
-    list->Add("a");
-    list->Add("b");
-    list->Add("c");
-    if (list->Size() != 3) {
-        return false;
-    }
+  auto list = new List<std::string>;
+  list->Add("a");
+  list->Add("b");
+  list->Add("c");
+  if (list->Size() != 3) {
+    return false;
+  }
 
-    list->Add("d");
-    if (list->Size() != 4) {
-        return false;
-    }
+  list->Add("d");
+  if (list->Size() != 4) {
+    return false;
+  }
 
-    list->Remove(2);
-    if (list->Size() != 3) {
-        return false;
-    }
+  list->Remove(2);
+  if (list->Size() != 3) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 int main() {
-    if (!testAdd1()) {
-        std::cout << "testAdd1 failed" << std::endl;
-    }
-    if (!testAdd2()) {
-        std::cout << "testAdd2 failed" << std::endl;
-    }
-    if (!testContains()) {
-        std::cout << "testContains failed" << std::endl;
-    }
-    if (!testIsEmpty()) {
-        std::cout << "testIsEmpty failed" << std::endl;
-    }
-    if (!testRemove()) {
-        std::cout << "testRemove failed" << std::endl;
-    }
-    if (!testSet()) {
-        std::cout << "testSet failed" << std::endl;
-    }
-    if (!testSize()) {
-        std::cout << "testSize failed" << std::endl;
-    }
+  if (!testAdd1()) {
+    std::cout << "testAdd1 failed" << std::endl;
+  }
+  if (!testAdd2()) {
+    std::cout << "testAdd2 failed" << std::endl;
+  }
+  if (!testContains()) {
+    std::cout << "testContains failed" << std::endl;
+  }
+  if (!testIsEmpty()) {
+    std::cout << "testIsEmpty failed" << std::endl;
+  }
+  if (!testRemove()) {
+    std::cout << "testRemove failed" << std::endl;
+  }
+  if (!testSet()) {
+    std::cout << "testSet failed" << std::endl;
+  }
+  if (!testSize()) {
+    std::cout << "testSize failed" << std::endl;
+  }
 }
