c5ecf133b6a01a05908fc9a508ee39719e04e244
Hidetatsu Yaginuma <deetyler@protonmail.com>
2020-07-31 01:51:55 +0900

    fix wrong comment

--- leetcode/202007/30/word_break_ii.go
+++ leetcode/202007/30/word_break_ii.go
@@ -14,36 +14,30 @@ func wordBreak(s string, wordDict []string) []string {
 	if len(wordDict) == 0 {
 		return []string{}
 	}
-
 	memo = map[string][]string{}
 	return helper(s, wordDict)
 }
-
 func helper(s string, dict []string) []string {
 	if m, ok := memo[s]; ok {
 		return m
 	}
-
 	if s == "" {
 		return nil
 	}
-
 	ret := []string{}
-
 	for _, word := range dict {
 		// when s == "catsanddog", dict = ["cat", "cats", "and", "sand", "dog"]
 		// first, find leading "cat"
 		if strings.HasPrefix(s, word) {
 			// then, call this function again with s == "sanddog"
 			rest := helper(strings.Replace(s, word, "", 1), dict)
-
-			// nil means the "word" is not found in s
+			// nil means the sentence is successfully created
 			if rest == nil {
 				ret = append(ret, word)
 				continue
 			}
-
 			// after recursion, rest should be like ["sand dog"]
+			// len(rest) can be 0 when the sentence couldn't be created
 			for _, w := range rest {
 				// ret will be "cat sand dog"
 				ret = append(ret, fmt.Sprintf("%s %s", word, w))
