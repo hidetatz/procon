8654d2d98fff5f53ebab7304aeebec6054add158
Hidetatz Yaginuma <hidetatz@gmail.com>
2022-04-03 15:31:15 +0900

    add template

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// using ll = long long;
+
+int main() {
+  // int n;
+  // cin >> n;
+  //
+  // vector<int> as(n);
+  // for (int i = 0; i < n; i++) cin >> as[i];
+  //
+  // for (int i = 0; i < n; i++) cout << as[i] << " ";
+  // cout << endl;
+  //
+  // cout << ans << endl;
+}
